ef35386d9d95a4a7e191ff401707f2ad82943394
jeff25lara <jeff25lara@gmail.com>
2023-01-20 11:29:18 -0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int a = 0; a < 24; a++) {
+
+            for (int b = 1; b <60; b++) {
+
+                if (b<10) {
+                    System.out.println(a + ":" + "0" + b);
+                }else
+                    System.out.println(a + ":" + b);
+            }
+        }
+    }
+}
